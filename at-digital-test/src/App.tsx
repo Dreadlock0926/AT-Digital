3746df213047d51de339d9f98681cb372eded41d
Dreadlock0926 <105129406+Dreadlock0926@users.noreply.github.com>
2024-04-16 19:10:05 +0530

    hero slide added

--- at-digital-test/src/App.tsx
+++ at-digital-test/src/App.tsx
@@ -1,15 +1,27 @@
-import Footer from "./components/Footer"
-import NavBar from "./components/NavBar"
+import Footer from "./components/Footer";
+import NavBar from "./components/NavBar";
+import hero from "./assets/hero.jpg";
+import "./App.css";
+import ConsultationButton from "./components/ConsultationButton";
 
 function App() {
-
-
   return (
     <div className="App">
       <NavBar />
+      <div className="app-body">
+        <div className="hero">
+          <div className="consultation-tab">
+            <h1>
+              We crush your competitors, goals, and sales records - without the
+              B.S.
+            </h1>
+            <ConsultationButton />
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ at-digital-test/src/components/ConsultationButton.tsx
@@ -0,0 +1,9 @@
+import "./ConsultationButton.css";
+
+function consultationButton() {
+  return (
+    <button className="consultation-btn">Get free consultation</button>
+  )
+}
+
+export default consultationButton;
